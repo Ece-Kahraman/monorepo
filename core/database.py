af6ffe5ee9141a7d0a5d78eb42dc32cc3629292c
Ece Kahraman <kahramanecoo@gmail.com>
2025-01-29 18:47:02 +0300

    chore: directory structure and db connection
    - set the expected directory structure
    - requirements.txt is set
    - other txt files are placeholders, will be deleted once I start implementing in their places
    - database connection is set
    - database credentials are set in .env

---
+++ core/database.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+_URL = "postgresql://" + os.environ.get('_DATABASE_URL')
+engine = create_engine(_URL)
