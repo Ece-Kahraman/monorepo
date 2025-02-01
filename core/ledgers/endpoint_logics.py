20419709c7f99fa32b2ec3caf7187b8e428732a5
Ece Kahraman <kahramanecoo@gmail.com>
2025-02-01 15:24:54 +0300

    fix: session warnings
    - found the correct way to inject from the fastapi page
    - mypy was begging me to use square brackets

--- core/ledgers/endpoint_logics.py
+++ core/ledgers/endpoint_logics.py
@@ -1,4 +1,6 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from core.database import get_db
 from core.ledgers.services import LedgerService
 
@@ -9,7 +11,7 @@ def reach_endpoints() -> APIRouter:  # take app specific parameters like its con
     ledger_logic = LedgerService()
 
     @ep.get("/ledger/{owner_id}")
-    def get_owner_balance(owner_id: str, session: Depends(get_db)):
+    def get_owner_balance(owner_id: str, session: Annotated[Session, Depends[get_db]]):
         return {"balance": ledger_logic.get_balance(session, owner_id)}
     
     return ep
