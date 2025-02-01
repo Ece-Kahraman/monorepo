17d5ec800381b15c5849c717c42b74ab2c75a3ec
Ece Kahraman <kahramanecoo@gmail.com>
2025-02-01 15:40:19 +0300

    add: core logic for the POST endpoint
    - reformatted (i really dislike how Black formats the parameter lists)

--- core/ledgers/endpoint_logics.py
+++ core/ledgers/endpoint_logics.py
@@ -3,15 +3,31 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from core.database import get_db
 from core.ledgers.services import LedgerService
+from core.ledgers.schemas import LedgerEntrySchema
 
 
-def reach_endpoints() -> APIRouter:  # take app specific parameters like its config dict
+def reach_endpoints(app_config: dict[str, int]) -> APIRouter:
 
     ep = APIRouter()
     ledger_logic = LedgerService()
 
     @ep.get("/ledger/{owner_id}")
-    def get_owner_balance(owner_id: str, session: Annotated[Session, Depends[get_db]]):
-        return {"balance": ledger_logic.get_balance(session, owner_id)}
-    
+    def get_owner_balance(
+        owner_id: str, db_session: Annotated[Session, Depends[get_db]]
+    ):
+        return {"balance": ledger_logic.get_balance(db_session, owner_id)}
+
+    @ep.post("/ledger")
+    def post_new_ledger(
+        new_ledger: LedgerEntrySchema, db_session: Annotated[Session, Depends[get_db]]
+    ):
+        new_entry_id = ledger_logic.post_ledger(
+            db_session,
+            new_ledger.operation.value,
+            new_ledger.nonce,
+            new_ledger.owner_id,
+            app_config,
+        )
+        return {"new ledger id": new_entry_id}
+
     return ep
