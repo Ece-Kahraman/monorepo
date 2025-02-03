edeb9f86aaf7ca7597f513d0effec189e41f982e
Ece Kahraman <kahramanecoo@gmail.com>
2025-02-03 04:46:30 +0300

    Update models.py

--- core/ledgers/models.py
+++ core/ledgers/models.py
@@ -3,11 +3,12 @@ from core.database import Base
 from core.ledgers.schemas import LedgerOperation
 
 
+"""Database model representing a ledger entry"""
 class LedgerEntryModel(Base):
     __tablename__ = "ledger_entries"
-    id = Column(Integer, primary_key=True)
-    operation = Column(type_=Enum(LedgerOperation), nullable=False)
-    amount = Column(Integer, nullable=False)
-    nonce = Column(String, nullable=False)
-    owner_id = Column(String, nullable=False)
-    created_on = Column(DateTime, nullable=False)
+    id = Column(Integer, primary_key=True) # Primary key
+    operation = Column(type_=Enum(LedgerOperation), nullable=False) # Enum-constrained operation type
+    amount = Column(Integer, nullable=False) # Operation credit amount
+    nonce = Column(String, nullable=False) # Unique transaction ID
+    owner_id = Column(String, nullable=False) # User identifier
+    created_on = Column(DateTime, nullable=False) # Automatic timestamping
